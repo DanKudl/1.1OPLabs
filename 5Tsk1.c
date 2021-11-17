723d40b6627074901d0cceadf19e4ad5c3b3a772
DanKudl <54040135+DanKudl@users.noreply.github.com>
2021-11-17 14:44:32 +0200

    Update 5Tsk1.c

--- 5Tsk1.c
+++ 5Tsk1.c
@@ -1,37 +1,76 @@
+
+/////////////////////////////////////////////////////////main.c
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "functions.h"
 
 
-double F(double x) {
-    return x / (x * x + 3);
+int main() {
+
+    double a, b, h;
+    printf("a, b: ");
+    scanf("%lf %lf", &a, &b);
+
+    printf("h: ");
+    scanf("%lf", &h);
+
+    tab1(a, b, h);
+    tab2(a, b, h);
+
+    return 0;
 }
 
-int F2(double y) {
-    if (y < 0) return -1;
-    else if (y == 0) return 0;
-    else return 1;
+
+///////////////////////////////////////////////////////////////////functions.c
+#include <stdio.h>
+#include <math.h>
+#include "functions.h"
+
+double f(double x) {
+	return x / (x * x + 3);
 }
-
-void Lab5Tsk1Var21() {
-    printf("y=x/(x^2 + 3)\n");
-
-    double y = 0, a = 1.0, b = 3.0, h = 0.2;
-
-    printf("h = "); scanf("%lf", &h);
-
-    printf("  X     Y           X     F2  \n");
-
-    for (double x = a; x <= b; x += h) {
-        printf("%5.3lf  ", x);
-        printf("%5.3lf      ", F(x));
-        printf("%5.3lf  ", x);
-        printf("%3d\n", F2(F(x)));
-        
-
-    }
+double y(double x, double* ptr) {
+	*ptr = f(x);
+	if (*ptr > 0)
+		return 1;
+	if (*ptr < 0)
+		return -1;
+	return 0;
 
 }
-
-int main () {
-  Lab5Tsk1Var21();
-  return 0;
+void tab1(double a, double b, double h) {
+	printf("\n X\t\t First method\n");
+	while (a <= b || fabs(b - a) < 1E-14)
+	{
+		printf("% .7lf\t% .7lf\n", a, f(a));
+		a += h;
+	}
 }
+
+void tab2(double a, double b, double h) {
+	printf("\n X\t\t First method\t Second method\n \n");
+	while (a <= b || fabs(b - a) < 1E-14)
+	{
+		double F, Y = y(a, &F);
+		printf("% .7lf\t% .7lf\t% .7lf\n", a, F, Y);
+		a += h;
+	}
+}
+
+
+////////////////////////////////////////////////////////functions.h
+
+#ifndef FUNCTIONS_H_   /* Include guard */
+#define FUNCTIONS_H_
+
+double f(double);
+double y(double, double*);
+void tab1(double, double, double);
+void tab2(double, double, double);
+
+#endif // FOO_H_
