04cd1f34d13da6519a717ac864c2c28afabd9850
DanKudl <54040135+DanKudl@users.noreply.github.com>
2021-11-24 13:44:38 +0200

    Update 6Tsk1.c

--- 6Tsk1.c
+++ 6Tsk1.c
@@ -1,3 +1,5 @@
+///////////////////////////////////////main.c
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <time.h>
@@ -8,8 +10,33 @@
 #include "functions.h"
 
 #define n 3
+#define LENGTH 1024
+#define N 3
 
-void input(double a[], double b[], double c[]) {
+
+
+int main() {
+    double a[n], b[n], c[n], sum1[n], d[n];
+    input(a, b, c, n);
+    sum(a, b, sum1, n);
+    sum(sum1, c, d, n);
+    printf("\nResult: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%6.2lf ", d[i]);
+    }
+    return 0;
+}
+
+
+//////////////////////////////////////////functions.c
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include "functions.h"
+
+
+void input(double a[], double b[], double c[], int n) {
     printf("a array elements(%d): ", n);
     for (int i = 0; i < n; i++) {
         scanf("%lf", &a[i]);
@@ -22,23 +49,24 @@ void input(double a[], double b[], double c[]) {
     for (int i = 0; i < n; i++) {
         scanf("%lf", &c[i]);
     }
-
 }
 
-void sum(double x[], double y[], double z[]) {
+void sum(double x[], double y[], double z[], int n) {
     for (int i = 0; i < n; i++) {
         z[i] = x[i] + y[i];
     }
 }
 
-int main() {
-    double a[n], b[n], c[n], sum1[n], d[n];
-    input(a, b, c);
-    sum(a, b, sum1);
-    sum(sum1, c, d);
-    printf("Result:              ");
-    for (int i = 0; i < n; i++) {
-        printf("%6.2lf ", d[i]);
-    }
-    return 0;
-}
+
+
+
+/////////////////////////////////////////functions.h
+
+#ifndef FUNCTIONS_H_ 
+#define FUNCTIONS_H_
+
+void input(double a[], double b[], double c[], int n);
+void sum(double x[], double y[], double z[], int n);
+
+#endif
+
