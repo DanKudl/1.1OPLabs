4aedcff6888d44830708f26936ae158c1403dc67
DanKudl <54040135+DanKudl@users.noreply.github.com>
2021-12-10 19:42:50 +0200

    Update 9Tsk1.c

--- 9Tsk1.c
+++ 9Tsk1.c
@@ -1,18 +1,32 @@
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define STRLEN 1024
 
-int main() {
+
+void output(int** p, int n) {
+	printf("\n\n");
+	for (int i = 0; i < n; i++) {
+		printf("array %d: ", i + 1, p[i][0]);
+
+		for (int j = 0; j < p[i][0]; j++) {
+			printf("%+d ", p[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main(int argc, char* argv[]) {
 	//////////Ввести не більше 5 масивів цілих чисел. Кількість чисел у масиві задається під час
 	//виконання програми, це число записати першим елементом масиву.Масиви розмістити в
 	///	динамічній пам'яті. Створити масив вказівників на дані масиви. У функції вивести на екран
 	///	всі елементи кожного масиву.
-	int N = -1;
-	while (N < 0 || N > 5) {
-		printf("arrays ammount: "); scanf("%d", &N);
-	}
+	
+	printf("amount of arrays: %s\n", argv[1]);
+	int N = atoi(argv[1]);
+	
 	int** arr = (int**)calloc(N, sizeof(int*));
 	int** p = (int**) calloc(N, sizeof(int*));
 	int size;
@@ -23,18 +37,20 @@ int main() {
 		p[i] = arr[i];
 		arr[i][0] = size;
 		for (int j = 1; j < size; j++) {
-			printf("%d/%d(%d array) element: ", j, size, i + 1);
+			printf("%d/%d(%d array) element: ", j + 1, size, i + 1);
 			scanf("%d", &arr[i][j]);
 		}
 	}
-	printf("\n\n");
+	output(p, N);
+	free(arr);
+	free(p);
 	for (int i = 0; i < N; i++) {
-		printf("array %d: ", i + 1);
-		for (int j = 0; j < arr[i][0]; p[i]++, j++) {
-			printf("%d ", *p[i]);
-		}
-		printf("\n");
+		free(arr[i]);
 	}
-  return 0;
+	arr = NULL;
+	p = NULL;
+	return 0;
 }
+
+
 
